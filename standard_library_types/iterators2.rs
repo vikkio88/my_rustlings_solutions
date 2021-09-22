200a0ee5187f4d38ce56ef2caf0c4261ad88c46f
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-09-22 21:43:51 +0100

    solutions to iterators, hated most of those, especially 3

--- standard_library_types/iterators2.rs
+++ standard_library_types/iterators2.rs
@@ -3,17 +3,24 @@
 // can offer. Follow the steps to complete the exercise.
 // As always, there are hints if you execute `rustlings hint iterators2`!
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
-    match c.next() {
+    let first = match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().to_string(),
+    };
+
+    let mut result = String::new();
+    result.push_str(&first);
+
+    for l in c {
+        result.push(l);
     }
+
+    result
 }
 
 // Step 2.
@@ -21,7 +28,12 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut results = Vec::new();
+    for word in words {
+        results.push(capitalize_first(word));
+    }
+
+    results
 }
 
 // Step 3.
@@ -29,7 +41,12 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut result = String::new();
+    for word in capitalize_words_vector(words) {
+        result.push_str(&word);
+    }
+
+    result
 }
 
 #[cfg(test)]
--- standard_library_types/iterators5.rs
+++ standard_library_types/iterators5.rs
@@ -1,4 +1,5 @@
 // iterators5.rs
+
 // Let's define a simple model to track Rustlings exercise progress. Progress
 // will be modelled using a hash map. The name of the exercise is the key and
 // the progress is the value. Two counting functions were created to count the
@@ -9,8 +10,6 @@
 // Execute `rustlings hint iterators5` for hints.
 //
 // Make the code compile and the tests pass.
-
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -34,6 +33,14 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
+    let mut count = 0;
+    for (_, v) in map.iter() {
+        if *v == value {
+            count += 1;
+        }
+    }
+
+    count
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -52,6 +59,13 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
+
+    let mut count = 0;
+    for c in collection {
+        count += count_iterator(c, value);
+    }
+
+    count
 }
 
 #[cfg(test)]
